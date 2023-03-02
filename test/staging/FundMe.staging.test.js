a7f32f80333c9ef14b9e30c65c08186acd2fff83
acidrums <edoardo.carradori.96@gmail.com>
2023-03-02 11:49:09 +0100

    Staging Tests

---
+++ test/staging/FundMe.staging.test.js
@@ -0,0 +1,23 @@
+const { assert } = require("chai")
+const { getNamedAccounts, ethers, network } = require("hardhat")
+const { developmentChains } = require("../../helper-hardhat-config")
+
+// Smart utilize of ternary operator
+developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("FundMe", async () => {
+        let fundMe
+        let deployer
+        const sendValue = ethers.utils.parseEther("1")
+        beforeEach(async () => {
+            deployer = (await getNamedAccounts()).deployer
+            fundMe = await ethers.getContract("FundMe", deployer)
+        })
+
+        it("allows people to fund and withdraw", async () => {
+            await fundMe.fund({ value: sendValue })
+            await fundMe.withdraw()
+            const endingBalance = await fundMe.provider.getBalance(fundMe.addrress)
+            assert.equal(endingBalance.toString(), "0")
+        })
+    })
